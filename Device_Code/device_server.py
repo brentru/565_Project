4341289b6cce37ba5310a5ca7e8b1e3332540526
brentru <robots199Wme.com>
2020-11-10 15:25:35 -0500

    refactor, add a dispatch method, add sensor ID constructor

--- Device_Code/device_server.py
+++ Device_Code/device_server.py
@@ -1,7 +1,8 @@
-import socketserver
-import socket
-import threading
 import logging
+import socket
+import socketserver
+import subprocess
+import threading
 import xml.etree.ElementTree as ET
 
 # Configuration #
@@ -17,6 +18,21 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(name)s: %(message)s',
                     )
 
+# Sensor IDs
+SENSOR_TEMP      = 1
+SENSOR_HUMID     = 2
+SENSOR_CPU_USAGE = 3
+SENSOR_CPU_TEMP  = 4
+SENSOR_DUMMY     = 5
+
+def dispatch_sensor_process(sensor_id):
+    """Dispatches a sensor process based on the sensor
+    identifier.
+    :param int sensor_id: Sensor type.
+
+    """
+    print("Dispatching sensor: ", NODE_SENSORS[int(sensor_id)])
+
 class RPC_Handler(socketserver.BaseRequestHandler):
     """Request handler class for a RPC_Server. 
     Handles communication with a connected TCP client.
@@ -28,19 +44,16 @@ class RPC_Handler(socketserver.BaseRequestHandler):
         :param str xml_cmd_string: XML message.
 
         """
-        # 
         msg_id = None
         msg_result = None
         msg_sensor_id = None
         msg_pid = None
 
-        try:
+        try: # attempt to parse XML command from interface
             root = ET.fromstring(xml_cmd_string)
         except:
             print("ERROR: Unable to parse command!")
             return False
-        print('Message Fields: ', len(root))
-
         # Parse out XML message
         for child in root:
             if child.tag == "id":
@@ -54,12 +67,13 @@ class RPC_Handler(socketserver.BaseRequestHandler):
                 elif "sensorid" in child.attrib['name']:
                     msg_sensor_id = child.text
                     print("Message sensorID: ", msg_sensor_id)
+                    # Dispatch a sensor process with sensor id
+                    dispatch_sensor_process(msg_sensor_id)
                 elif "pid" in child.attrib['name']:
                     msg_pid = child.text
                     print("Message PID: ", msg_pid)
                 else:
                     print("ERROR: Unexpected message field.")
-
         return True
 
     def handle(self):
@@ -87,10 +101,19 @@ class RPC_Handler(socketserver.BaseRequestHandler):
             if b'quit' in _data:
                 close = 1
 
-if __name__ == '__main__':
-    import socket
-    server_address = (SERVER_ADDRESS, SERVER_PORT)
-
+def start_server():
     with socketserver.TCPServer(server_address, RPC_Handler) as server:
         print("Server Active on {}:{}".format(SERVER_ADDRESS, SERVER_PORT))
         server.serve_forever()
+
+if __name__ == '__main__':
+    # Create a list of sensors attached to the node
+    NODE_SENSORS = [SENSOR_TEMP, SENSOR_HUMID,
+                    SENSOR_CPU_USAGE, SENSOR_CPU_TEMP,SENSOR_DUMMY]
+
+    # Define server address
+    server_address = (SERVER_ADDRESS, SERVER_PORT)
+
+    # Create and initialize server
+    # NOTE: This starts a blocking loop
+    start_server()
